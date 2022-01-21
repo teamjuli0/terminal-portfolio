2dd9805e3812805e74f3cc1db83e15d329dcfadd
Julio Valdez <valdezjulio95@gmail.com>
2022-01-20 19:21:33 -0600

    comment on main

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Router>
         <Switch>
           {/* comment to do something for something idk */}
+          {/* comment on main */}
           <Route exact path='/' component={LandingPage} />
         </Switch>
       </Router>
